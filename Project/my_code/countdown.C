660755754babda64d0f3649cbb3f84a0725d4911
cook <yuanxiaojie6616@163.com>
2012-07-18 17:04:34 +0800

    + ‘Project/my_code/countdown.C’

---
+++ Project/my_code/countdown.C
@@ -0,0 +1,29 @@
+#include<iostream.h>
+#include<conio.h>
+main()
+{
+    int m,s;
+    cout<<"\n\n\n\t\t\t A COUNTDOWN TIMER \n";
+    cout<<enter time in MINUTES here \t = \t";
+    cin>>m;
+    cout<<enter time in SECONDS here \t = \t";
+    cin>>s;
+    cout<<"\n\n\n\n\t press any key to start...!!!";
+    getch();
+    clrscr();
+    cout<<"\n\n\n\t\t\t A COUNTDOWN TIMER \n";
+    cout<<"\n\n\n\t\t TIME REMAINIG \n\n";
+    cout<<"\t mins: \t\t seconds: ";
+    for(int min=m;min>0;min--) {
+	    for(int sec=59;sec>=;sec--) {
+		    sleep(1);
+	    	cout<<"\r"<<min<<"\t"<<sec;
+		}
+    }
+	for(int rem=s;rem>=0;rem--) {
+		cout<<"\r"<<min<<"\t"<<rem;
+	}
+
+	cout<<"\n\n ENDED ";
+	return 0;
+	} 
