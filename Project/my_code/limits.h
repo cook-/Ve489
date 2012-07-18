76d30d0cbbabce3bff7297255b901edd742cb384
cook <yuanxiaojie6616@163.com>
2012-07-18 21:11:53 +0800

    / ‘Project/my_code/limits.h’

--- Project/my_code/limits.h
+++ Project/my_code/limits.h
@@ -1,7 +1,7 @@
 #ifndef LIMITS_H
 #define LIMITS_H
 
-const unsigned int MAX_USER_NUMBER = 10;
+const unsigned int MAX_USER_NUMBER = 2;
 const unsigned int MAX_FRAME_LENGTH = 100;
 //const unsigned int 
 
