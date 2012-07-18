a41d405c312ce9b19ac0a969b297781092336b43
cook <yuanxiaojie6616@163.com>
2012-07-18 22:06:14 +0800

    / ‘Project/my_code/main.C’

--- Project/my_code/main.C
+++ Project/my_code/main.C
@@ -7,12 +7,12 @@ using namespace std;
 int
 main()
 {
-	int *a[MAX_USER_NUM][MAX_TIME_SLOT];
+	int a[MAX_USER_NUM][MAX_TIME_SLOT];
 	double p = 0.3;
-	srand(time(NULL));
+	srand((unsigned)time(NULL));
 	for (int i = 0; i != MAX_USER_NUM; ++i) {
 		for (int j = 0; j != MAX_TIME_SLOT; ++j) {
-			if (rand() < (RAND_MAX + 1)*p) {
+			if (rand() < RAND_MAX*p) {
 				for (int k = 0; k != MAX_FRAME_LEN && j != MAX_TIME_SLOT; ++k) {
 					*(a+i*(MAX_TIME_SLOT)+j) = 1;
 					++j;
