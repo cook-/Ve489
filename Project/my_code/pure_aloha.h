dd60bb1a9d8df0bfbf4936f84e28e4e7ed23bd7d
cook <yuanxiaojie6616@163.com>
2012-07-18 20:42:02 +0800

    + ‘Project/my_code/pure_aloha.h’

---
+++ Project/my_code/pure_aloha.h
@@ -0,0 +1,27 @@
+#ifndef PURE_ALOHA_H
+#define PURE_ALOHA_H
+
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+void
+initUsers();
+
+void
+generateFrame();
+
+void
+checkCollision();
+
+void
+waitForRandomTime();
+
+
+
+
+
+
+
+#endif
