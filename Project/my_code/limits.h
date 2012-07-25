71e58fb94079ad9fd8d0cbe48b1f3cb708a80c18
cook <yuanxiaojie6616@163.com>
2012-07-25 17:33:56 +0800

    / ‘Project/my_code/limits.h’

--- Project/my_code/limits.h
+++ Project/my_code/limits.h
@@ -1,6 +1,9 @@
 #ifndef LIMITS_H
 #define LIMITS_H
 
+#include <cstdlib>
+using namespace std;
+
 const unsigned int MAX_USER_NUM = 5;
 const unsigned int MAX_SLOT_NUM = 30;
 const unsigned int MAX_FRAME_LEN = 4;
