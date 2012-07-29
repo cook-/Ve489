ee678de6221548725c59cdca358e389695f2fb4b
cook <yuanxiaojie6616@163.com>
2012-07-30 01:35:05 +0800

    / ‘Project/my_code/a.out’ / ‘Project/my_code/main.C’

--- Project/my_code/main.C
+++ Project/my_code/main.C
@@ -4,7 +4,7 @@
 using namespace std;
 
 const unsigned int USER_NUM = 5;
-const unsigned int SIMULATE_TIME = 40;
+const unsigned int SIMULATE_TIME = 100;
 const unsigned int FRAME_LEN = 4;
 const unsigned int MAX_WAIT_TIME = RAND_MAX;
 const int COLLISION = -1;
