e9b02e8373a919e4bdb97fea08941bf14c4c9d04
cook <yuanxiaojie6616@163.com>
2012-07-29 20:15:18 +0800

    / ‘Project/my_code/a.out’ / ‘Project/my_code/main.C’

--- Project/my_code/main.C
+++ Project/my_code/main.C
@@ -27,47 +27,6 @@ main()
 	// a short time interval.
 	double p = 0.03;
 
-/*	// generate frames by probability p.
-	srand((unsigned)time(NULL));
-	for (int i = 0; i != MAX_USER_NUM; ++i) {
-		for (int j = 0; j != MAX_SLOT_NUM; ++j) {
-			if (rand() < RAND_MAX*p) {
-				for (int k = 0; k != FRAME_LEN && j != MAX_SLOT_NUM; ++k) {
-					a[i][j] = 1;
-					++j;
-				}
-				--j;
-			}
-			else
-				a[i][j] = 0;
-		 }
-	}
-
-
-	for (int i = 0; i != MAX_USER_NUM; ++i) {
-		cout << "user " << i + 1 << ":\t";
-		for (int j = 0; j != MAX_SLOT_NUM; ++j) {
-			cout << a[i][j] << " ";
-		}
-		cout << endl;
-	}
-	
-	// check for collision.
-	// arrays storing COLLISION/SUCCESS/IDLE for each user.
-	
-	for (int i = 0; i != MAX_SLOT_NUM; ++i) {
-		int cnt = 0;
-		for (int j = 0; j != MAX_USER_NUM; ++j) {
-			if (a[i][j] == 1)
-				++cnt;
-		}
-		if (cnt > 1) {
-		// wait for random time.
-			for (int j = 0; )
-		}
-	}
-*/
-
 	return 0;
 }
 
