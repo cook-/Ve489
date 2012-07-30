c6722d67263c7c871ac4a2beb3cb769829e1f515
cook <yuanxiaojie6616@163.com>
2012-07-30 18:43:55 +0800

    / ‘Project/my_code/main.C’

--- Project/my_code/main.C
+++ Project/my_code/main.C
@@ -27,7 +27,7 @@ main()
 
 	// the probability for a single user to generate a frame during 
 	// a short time interval.
-	double p = 0.99;
+	double p = 0.01;
 
 	pure_aloha_simulate(a, p);
 	slotted_aloha_simulate(a, p);
