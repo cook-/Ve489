ed7921b75c7417d05d3313f89f6814d71fe9cd04
cook <yuanxiaojie6616@163.com>
2012-07-30 10:56:49 +0800

    / ‘Project/my_code/a.out’ / ‘Project/my_code/main.C’

--- Project/my_code/main.C
+++ Project/my_code/main.C
@@ -3,7 +3,7 @@
 #include <ctime>
 using namespace std;
 
-const unsigned int USER_NUM = 20;
+const unsigned int USER_NUM = 10;
 const unsigned int SIMULATE_TIME = 1000;
 const unsigned int FRAME_LEN = 4;
 const unsigned int MAX_WAIT_TIME = RAND_MAX;
@@ -26,7 +26,7 @@ main()
 
 	// the probability for a single user to generate a frame during 
 	// a short time interval.
-	double p = 0.50;
+	double p = 0.9;
 
 //	for (int i = 0; i != 1000; ++i)
 		pure_aloha_simulate(a, p);
